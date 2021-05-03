e0578c6f086cb9dfb656f19329d4e3dd6e454684
Henrique Poleselo <hpoleselo@gmail.com>
2021-05-03 07:12:03 -0300

    Update

--- electric-power-systems/fault-work/tensao-de-falta.py
+++ electric-power-systems/fault-work/tensao-de-falta.py
@@ -1,7 +1,10 @@
 import numpy as np
-from copy import deepcopy
+from copy import deepcopy, copy
 from math import pi, sin, cos
 import pandas as pd
+
+
+
 
 def ret2pol(x, y):
     modulo = np.sqrt(x**2 + y**2)
@@ -10,7 +13,7 @@ def ret2pol(x, y):
     return (modulo, theta)
 
 def pol2ret(modulo, theta):
-    # Converter para radianos, já que Python usa rad
+    # Converter para radianos, já que Python usa rad internamente
     theta = theta*(pi/180)
     x = modulo * np.cos(theta)
     y = modulo * np.sin(theta)
@@ -93,58 +96,23 @@ def gerarMatrizesDefasamento():
     matrizDefasagemFinal[6] = VetDefasagemB
     matrizDefasagemFinal[7] = VetDefasagemC
 
-    # TODO: adicionar explicação
-
-    # Falta na Barra 4
-    VetDefasagem_a = np.zeros(10, dtype=complex)
-    VetDefasagem_a[1] = defasamento0Grau
-    VetDefasagem_a[2] = defasamento30Graus
-    VetDefasagem_a[3] = defasamento0Grau
-    VetDefasagem_a[4] = defasamento30Graus
-    VetDefasagem_a[5] = defasamento0Grau
-    VetDefasagem_a[6] = defasamento0Grau 
-    VetDefasagem_a[7] = defasamento90GrausNeg
-    VetDefasagem_a[8] = defasamento30Graus
-    VetDefasagem_a[9] = defasamento30Graus
-
-    # Falta na Barra 6
-    VetDefasagem_b = np.zeros(10, dtype=complex)
-    VetDefasagem_b[1] = defasamento0Grau
-    VetDefasagem_b[2] = defasamento0Grau
-    VetDefasagem_b[3] = defasamento0Grau
-    VetDefasagem_b[4] = defasamento0Grau
-    VetDefasagem_b[5] = defasamento0Grau
-    VetDefasagem_b[6] = defasamento0Grau
-    VetDefasagem_b[7] = defasamento90GrausNeg
-    VetDefasagem_b[8] = defasamento30Graus
-    VetDefasagem_b[9] = defasamento30Graus
-
-    # Falta na Barra 7
-    VetDefasagem_c = np.zeros(10, dtype=complex)
-    VetDefasagem_c[1] = defasamento90GrausNeg
-    VetDefasagem_c[2] = defasamento120GrausNeg
-    VetDefasagem_c[3] = defasamento90GrausNeg
-    VetDefasagem_c[4] = defasamento120GrausNeg
-    VetDefasagem_c[5] = defasamento90GrausNeg
-    VetDefasagem_c[6] = defasamento90GrausNeg
-    VetDefasagem_c[7] = defasamento0Grau
-    VetDefasagem_c[8] = defasamento120Graus
-    VetDefasagem_c[9] = defasamento120Graus
-
-    matrizDefasagemInicial = np.zeros(100, dtype=complex)
-    matrizDefasagemInicial = matrizDefasagemInicial.reshape(10,10)
-    matrizDefasagemInicial[4] = VetDefasagem_a
-    matrizDefasagemInicial[6] = VetDefasagem_b
-    matrizDefasagemInicial[7] = VetDefasagem_c
-
     #mtiz = pd.DataFrame(matrizDefasagemFinal)
-    mtiz = pd.DataFrame(matrizDefasagemInicial)
-    print(mtiz)
-    #print(matrizDefasagemInicial)
+    #print(mtiz)
 
-    return matrizDefasagemFinal, matrizDefasagemInicial, VetDefasagem
+    return matrizDefasagemFinal, VetDefasagem
+
 
 #region Dados do Código Anterior
+
+LT01C1Z = 0.004589035916824197+0.05059251417769376j
+LT01C2Z = 0.004589035916824197+0.05059251417769376j
+LT02C1Z = 0.004627027935307709+0.05150148288174754j
+LT02C2Z = 0.004627027935307709+0.05150148288174754j
+LT03C1Z = 0.00542410417979416+0.05754327242176014j
+LT04C1Z = 0.005848452005881117+0.07352606595253099j
+LT05C1Z = 0.007037807183364839+0.08547561436672968j
+TR02T1Z = 0.12280000000000002j
+TR03T1Z = 0.1678j
 VBase1 = 13.8
 VBase2 = 345
 VBase3 = 4.16
@@ -157,10 +125,11 @@ ZB2 = 1190.25
 ZB3 = 0.17305600000000002
 ZB4 = 190.44
 ZB5 = 1.9044000000000003
-
 # Importando ZBarra calculado no programa anterior
 ZBarra = np.genfromtxt('zbarra.csv', dtype=complex, delimiter=',')
+
 #endregion
+
 
 # Corrente de Base
 IB1 = SBase/VBase1
@@ -194,18 +163,29 @@ VpreF[9] = pol2ret(1.0207,-8.8582)
 
 # Inicializando a corrente de falta
 If = np.zeros(10, dtype=complex)
-# Inicializando as tensões pós-falta
-Vpos = np.zeros(10, dtype=complex)
+# Inicializando as tensões pós-falta, deixamos uma dimensão maior para as correntes circumvizinhas
+Vpos = np.zeros(100, dtype=complex)
+Vpos = Vpos.reshape(10,10)
 
-matrizDefasagemFinal, matrizDefasagemInicial, VetDefasagem = gerarMatrizesDefasamento()
+# Inicializando as correntes circumvizinhas
+Iv = np.zeros(10, dtype=complex)
+Vpos_ = np.zeros(100, dtype=complex)
+Vpos_ = Vpos_.reshape(10,10)
+
+
+
+matrizDefasagemFinal, VetDefasagem = gerarMatrizesDefasamento()
+
+
 
 # Barras onde ocorrem as faltas
 barra = [4, 6, 7]
 
 for k in barra:
+
     print(f'\n\n\n######## Falta na Barra {k} ########')
     # Multiplicando pelo _VPos pois este possui as defasagens corretas
-    If[k] = (VpreF[k] * matrizDefasagemInicial[k,k]) / (Zf[k] + ZBarra[k-1,k-1]) # k-1 pois eh uma matriz 10x10
+    If[k] = (VpreF[k] * VetDefasagem[k]) / (Zf[k] + ZBarra[k-1,k-1]) # k-1 pois eh uma matriz 10x10
     re_ifk = If[k].real
     im_ifk = If[k].imag
     # Expondo em polar pois no Anafas é como sera inputado
@@ -215,21 +195,40 @@ for k in barra:
     for n in range(1,10):
 
         # Em relação a barra 4, escrever as tensões em relação a si propria
-        Vpos[n] = (VpreF[n] * matrizDefasagemInicial[k,n]) - (ZBarra[n-1,k-1] * If[k])
+        Vpos[k,n] = (VpreF[n] * VetDefasagem[n]) - (ZBarra[n-1,k-1] * If[k])
+        
+        # Copiando os valores das tensões pós-falta para o cálculo das correntes circumvizinhas (pois)
+        #Vpos_[k,n] = deepcopy(Vpos[k,n])
+        #print("CHECAGEM!!!!!!!!!!!")
+        #print(Vpos_[k,n])
 
-        Vpos[n] = Vpos[n] * matrizDefasagemFinal[k,n]
-        re_vpos = Vpos[n].real
-        im_vpos = Vpos[n].imag
+        #Vpos[k,n] = Vpos[k,n] * matrizDefasagemFinal[k,n]
+        re_vpos = Vpos[k,n].real
+        im_vpos = Vpos[k,n].imag
         Vpos_polar_r, Vpos_polar_theta = ret2pol(re_vpos, im_vpos)
 
         # TODO: Dar em PU e em Volts
         # VBase*
         print(f'\n Tensão pós-falta na barra {n} é: Módulo(PU): {Vpos_polar_r} Fase(graus): {Vpos_polar_theta}')
 
+
+
+
 #region -- Correntes Circuvizinhas --
 # Correntes adjacentes àos lugares das faltas
 # Calcular todas as ramificacoes de onde a falta se deu, da barra 4
 # para calcular a corrente circunvizinhas: tensao de pos falta entre as barras/impedancia
+
+# Calculando corrente vizinha entre a barra 4 e 2
+#Iv_4_2 = ( Vpos[4,4]*(np.conj(matrizDefasagemFinal[4,4])) - Vpos[4,2]*(np.conj(matrizDefasagemFinal[4,2])) ) / LT01C1Z
+Iv_4_2 = ( Vpos[4,4] - Vpos[4,2] ) / LT01C1Z
+re_vpos2 = Iv_4_2.real
+im_vpos2 = Iv_4_2.imag
+Iv_r, Iv_theta = ret2pol(re_vpos2, im_vpos2)
+
+# TODO: Dar em PU e em Volts
+# VBase*
+print(f'\n PIKA NO KU {n} é: Módulo(PU): {Iv_r} Fase(graus): {Iv_theta}')
 
 #Iv = (VposF4 - VposF3)/ZLTij
 #endregion
